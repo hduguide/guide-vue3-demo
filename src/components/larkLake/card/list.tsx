af33d331bae5aa87b2f475058885d8f7ae42fe0f
Jack Li <dreamer22qq@gmail.com>
2021-09-02 00:17:13 -0400

    fix: ordered list types

--- src/components/larkLake/card/list.tsx
+++ src/components/larkLake/card/list.tsx
@@ -1,9 +1,11 @@
 import { defineComponent, h } from 'vue'
 import { renderChildren } from '../utils'
-import { NodeTag, Attributes } from 'posthtml-parser'
+import { NodeTag } from 'posthtml-parser'
+import * as roman from 'romans'
 
 const liSymbols = ['●', '○', '■']
 const olTypes = ['1', 'a', 'i']
+type OrderedType = '1' | 'a' | 'i'
 
 const LakeUliNode = defineComponent({
   props: ['node'],
@@ -11,10 +13,11 @@ const LakeUliNode = defineComponent({
     const node = props.node as NodeTag
     const children = node.content as NodeTag[]
     const indent = (attrs['data-lake-indent'] as string) ?? '0'
+    const symbol = liSymbols[+indent % liSymbols.length]
     return () => (
       <ul {...node.attrs}>
         {children.map((child) => (
-          <LakeLiNode {...child.attrs} indent={indent}>
+          <LakeLiNode {...child.attrs} symbol={symbol}>
             {renderChildren(child)}
           </LakeLiNode>
         ))}
@@ -23,25 +26,54 @@ const LakeUliNode = defineComponent({
   }
 })
 
+function genOrderedSymbol(order: number, type: OrderedType) {
+  const genAorder = (order: number) => {
+    const base = 'zabcdefghijklmnopqrstuvwxyz'
+    const len = 26
+    let num = order
+    let result = []
+    while (num > 0) {
+      const mod = num % len
+      result.push(base[mod])
+      num = Math.floor(num / len) - (mod == 0 ? 1 : 0)
+    }
+    return result.reverse().join('')
+  }
+
+  switch (type) {
+    case '1':
+      return `${order}. `
+    case 'a':
+      return `${genAorder(order)}. `
+    case 'i':
+      return `${roman.romanize(order)}. `.toLowerCase()
+  }
+}
+
 const LakeOliNode = defineComponent({
   props: ['node'],
   setup(props, { attrs }) {
     const node = props.node as NodeTag
+    const children = node.content as NodeTag[]
     const indent = (attrs['data-lake-indent'] as string) ?? '0'
-    const type: any = olTypes[+indent % olTypes.length] ?? '1'
+    const type = (olTypes[+indent % olTypes.length] ?? '1') as OrderedType
+    const start = (attrs['start'] as string) ?? '1'
     return () => (
       <ol type={type} {...node.attrs}>
-        {() => renderChildren(node)}
+        {children.map((child, i) => (
+          <LakeLiNode symbol={genOrderedSymbol(+start + i, type)} {...child.attrs}>
+            {renderChildren(child)}
+          </LakeLiNode>
+        ))}
       </ol>
     )
   }
 })
 
 const LakeLiNode = defineComponent({
-  props: ['indent'],
+  props: ['symbol'],
   setup(props, { slots }) {
-    const { indent } = props
-    const symbol = liSymbols[+indent % liSymbols.length]
+    const { symbol } = props
     return () => (
       <li>
         <span>{symbol} </span>
